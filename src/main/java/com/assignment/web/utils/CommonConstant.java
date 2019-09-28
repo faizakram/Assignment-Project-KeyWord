8bbff1cc41fee01301577f02174052cd4f560973
Faiz Akram <faiz.akram@compunnel.in>
2019-09-28 16:29:33 +0530

    commted

--- src/main/java/com/assignment/web/utils/CommonConstant.java
+++ src/main/java/com/assignment/web/utils/CommonConstant.java
@@ -12,9 +12,6 @@ public class CommonConstant {
 	public static final String SEARCH = "/search";
 	public static final String PUBLICATION_URL = "/publication/v1";
 	public static final String ENUM_URL = "/enum/v1";
-	public static final String S3_URL = "/s3/v1";
-	public static final String S3_CREATE_BUCKET = "/createBucket";
-	public static final String UPLOAD_FILE = "/upload-file";
 	public static final String BASE_PACKAGE = "com.assignement";
 
 	public static final String SUCCESS_PROPERTIES = "classpath:success.properties";
@@ -56,20 +53,10 @@ public class CommonConstant {
 	public static final String E1008_ERROR_DESCRIPTION = "E1008-ERROR-DESCRIPTION";
 	public static final String E1009_ERROR_CODE = "E1009-ERROR-CODE";
 	public static final String E1009_ERROR_DESCRIPTION = "E1009-ERROR-DESCRIPTION";
-	public static final String E1010_ERROR_CODE = "E1010-ERROR-CODE";
-	public static final String E1010_ERROR_DESCRIPTION = "E1010-ERROR-DESCRIPTION";
-	public static final String E1011_ERROR_CODE = "E1011-ERROR-CODE";
-	public static final String E1011_ERROR_DESCRIPTION = "E1011-ERROR-DESCRIPTION";
-	public static final String E1012_ERROR_CODE = "E1012-ERROR-CODE";
-	public static final String E1012_ERROR_DESCRIPTION = "E1012-ERROR-DESCRIPTION";
 	//Success Message Constant
 	public static final String S0001_SUCCESS_CODE = "SM-HTTP_CODE-S0001";
 	public static final String S0001_SUCCESS_DESCRIPTION = "SM-HTTP_DESCRIPTION-S0001";
 	public static final String S0002_SUCCESS_DESCRIPTION = "SM-HTTP_DESCRIPTION-S0002";
 	public static final String S0003_SUCCESS_DESCRIPTION = "SM-HTTP_DESCRIPTION-S0003";
 	public static final String S0004_SUCCESS_DESCRIPTION = "SM-HTTP_DESCRIPTION-S0004";
-	public static final String S0005_SUCCESS_DESCRIPTION = "SM-HTTP_DESCRIPTION-S0005";
-	public static final String S0006_SUCCESS_DESCRIPTION = "SM-HTTP_DESCRIPTION-S0006";
-	public static final String S0007_SUCCESS_DESCRIPTION = "SM-HTTP_DESCRIPTION-S0007";
-	public static final String SLASH = "/";
 }
